180a3d38025cc560ff237c8f4171caf23dc83644
Andi Sidwell <andi@takkaria.org>
2015-01-06 18:50:44 +0000

    Add event calendar AJAXy forward/backward features

--- routes/events.js
+++ routes/events.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET events */
-router.get('/', function(req, res) {
+router.get('/*', function(req, res) {
 	res.render('events', { event: req.event });
 });
 
