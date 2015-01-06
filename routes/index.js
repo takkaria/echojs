4842cc369d273c332cad527a04143449d66470a5
Andi Sidwell <andi@takkaria.org>
2015-01-06 16:28:04 +0000

    Get front-page events displaying in date-groups

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,8 +12,38 @@ router.get('/', function(req, res) {
 			{ state: "approved" },
 			"(startdt >= date('now', 'start of day') OR date('now') <= enddt)"
 		],
-		limit: 10
+		limit: 10,
+		order: "startdt ASC"
 	}).then(function(events) {
+
+		// This will look like
+		// [ { date: moment, events: [ models.Event(), models.Event(), ... ] }, ... ]
+		var ordered = [];
+		var chunk;
+		var last;
+
+		// Group events by date
+		events.forEach(function(event) {
+			var date = event.startdt.format("YYYY-MM-DD");
+
+			// Create a new grouping ('chunk') for a different date
+			if (date != last) {
+				chunk = {};
+				chunk.date = moment(date);
+				chunk.longDate = chunk.date.format("dddd, Do MMMM YYYY");
+				chunk.events = [];
+				if (chunk.date.diff(moment(), 'days') < 7)
+					chunk.shortDate = chunk.date.calendar();
+
+				ordered.push(chunk);
+
+				// Remember date for next iteration
+				last = date;
+			}
+
+			// Add event to current chunk
+			chunk.events.push(event);
+		});
 
 		models.Post.findAll({
 			include: [ models.Feed ],
@@ -21,7 +52,7 @@ router.get('/', function(req, res) {
 				"WHERE p2.feed_id = post.feed_id AND p2.date > post.date) == 0",
 			order: "date DESC"
 		}).then(function(posts) {
-			res.render('index', { events: events, posts: posts });
+			res.render('index', { events: ordered, posts: posts });
 		});
 	});
 });
