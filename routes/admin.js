8d885ab4cefca0eab28b6a915f4b4b1e6a542182
Carl van Tonder <carl@supervacuo.com>
2015-01-08 23:55:37 +0000

    Working "approve" function / view / etc.
    RE #5

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,28 @@ var express = require('express'),
 	moment = require('moment'),
 	router = express.Router();
 
-/* GET event add */
-router.get('/', function(req, res) {
-	if(!req.isAuthenticated()) {
-		return res.redirect('/user/login?next=' + encodeURIComponent('/admin'));
-	}
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/user/login?next=' + encodeURIComponent(req.originalUrl))
+}
 
+router.param('id', function(req, res, next, id) {
+	var models = req.app.get('models');
+
+	models.Event.find({
+		where: { id: id }
+	}).then(function(event_) {
+		if (!event_)
+			return next(new Error("No such event"));
+
+		req.event_ = event_;
+	}).then(next, function (err) {
+		next(err);
+	});
+})
+
+/* GET event add */
+router.get('/', ensureAuthenticated, function(req, res) {
 	var models = req.app.get('models'),
 		sequelize = models.sequelize;
 
@@ -22,6 +38,47 @@ router.get('/', function(req, res) {
 			user: req.user,
 			events_: events_
 		});
+	});
+});
+
+router.get('/:id', ensureAuthenticated, function(req, res) {
+	res.render('event_page', {
+		event_: req.event_,
+		user: req.user
+	});
+});
+
+router.get('/:id/approve', ensureAuthenticated, function(req, res) {
+	var event_ = req.event_;
+	if (event_.state !== 'submitted') {
+		res.redirect('/admin/' + req.event_.id);
+	}
+	res.render('event_approve', {
+		event_: event_,
+		user: req.user
+	});
+});
+
+router.post('/:id/approve', ensureAuthenticated, function(req, res) {
+	var event_ = req.event_;
+	if (event_.state !== 'submitted') {
+		res.redirect('/admin/' + req.event_.id);
+	}
+	event_.set('state', 'approved');
+	event_.save().then(function(){
+		event_.reload();
+		console.log(event_.absolute_url);
+		res.redirect('/event/' + event_.id);  // FIXME should be event_.absolute_url
+	})
+	.catch(function(errors){
+		console.log(errors);
+	});
+});
+
+router.get('/:id/reject', ensureAuthenticated, function(req, res) {
+	res.render('event_reject', {
+		event_: req.event_,
+		user: req.user
 	});
 });
 
