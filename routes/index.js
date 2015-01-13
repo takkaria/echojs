3ae64d77c941c8c2d352fc6c792e854dae874e7a
Carl van Tonder <carl@supervacuo.com>
2015-01-13 21:45:05 +0000

    Move event grouping to Event.groupByDays()

--- routes/index.js
+++ routes/index.js
@@ -20,44 +20,14 @@ router.get('/', function(req, res) {
 	var models = req.app.get('models');
 	var sequelize = models.sequelize;
 
-	models.Event.findAll({
+	models.Event.groupByDays({
 		where: [
 			{ state: "approved" },
 			"(startdt >= date('now', 'start of day') OR date('now') <= enddt)"
 		],
 		limit: 10,
 		order: "startdt ASC"
-	}).then(function(events) {
-		// This will look like
-		// [ { date: moment, events: [ models.Event(), models.Event(), ... ] }, ... ]
-		var ordered = [];
-		var chunk;
-		var last;
-
-		// Group events by date
-		events.forEach(function(event) {
-			var date = event.startdt.format("YYYY-MM-DD");
-
-			// Create a new grouping ('chunk') for a different date
-			if (date != last) {
-				chunk = {};
-				chunk.date = moment(date);
-				chunk.longDate = chunk.date.format("dddd, Do MMMM YYYY");
-				chunk.events = [];
-
-				if (chunk.date.diff(moment(), 'days') < 7)
-					chunk.shortDate = chunk.date.calendar();
-
-				ordered.push(chunk);
-
-				// Remember date for next iteration
-				last = date;
-			}
-
-			// Add event to current chunk
-			chunk.events.push(event);
-		});
-
+	}, function(ordered){
 		models.Post.findAll({
 			include: [ models.Feed ],
 			where: "hidden IS NOT 1 AND date >= date('now', '-3 months') AND ( " +
