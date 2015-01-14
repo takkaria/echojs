c2831d55ed5fcaf3674974da58cb186708b01955
Carl van Tonder <carl@supervacuo.com>
2015-01-14 14:50:51 +0000

    Self-service password changes.
    Closes #32

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,11 @@ var express = require('express'),
 	passport = require('passport'),
 	router = express.Router();
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/user/login?next=' + encodeURIComponent(req.originalUrl))
+}
+
 router.param('email', function(req, res, next, id) {
 	var models = req.app.get('models');
 
@@ -25,7 +30,7 @@ router.get('/login', function(req, res) {
 router.post('/login', function(req, res, next) {
 	passport.authenticate('local', function(err, user, info) {
 		if (err) { return next(err); }
-		if (!user) { return res.redirect('/user/login'); }
+		if (!user) { return res.render('login'); }
 		req.logIn(user, function(err) {
 			if (err) { return next(err); }
 			console.log(req.body.next);
@@ -37,7 +42,47 @@ router.post('/login', function(req, res, next) {
 	})(req, res, next);
 });
 
-router.get('/logout', function(req, res){
+router.get('/password', ensureAuthenticated, function(req, res) {
+	res.render('user_password', {user: req.user});
+});
+
+router.post('/password', ensureAuthenticated, function(req, res, next) {
+	passport.authenticate('local', function(err, user, info) {
+		if (err) { return next(err); }
+		if (!user) { return res.render('user_password', {
+			user: req.user,
+			errors: [{
+				path: 'password',
+				message: 'Your current password doesn\'t seem to be correct; try again?'
+			}]
+		}); }
+		if(req.body.new_password === ''){
+			return res.render('user_password', {
+				user: req.user,
+				errors: [{
+					path: 'new_password',
+					message: 'New password can\'t be blank'
+				}]
+			});
+		}
+		if(req.body.new_password !== req.body.new_password2){
+			return res.render('user_password', {
+				user: req.user,
+				errors: [{
+					path: 'new_password2',
+					message: 'New passwords don\'t match'
+				}]
+			});
+		}
+		req.user.setPassword(req.body.new_password);
+		req.user.save().then(function(u_){
+			req.flash('success', 'Password changed')
+			return res.redirect('/');
+		});
+	})(req, res, next);
+});
+
+router.get('/logout', ensureAuthenticated, function(req, res){
 	req.logout();
 	res.redirect('/');
 });
