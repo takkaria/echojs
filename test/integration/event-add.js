e4c05d9a817b86a5dc566b6c4f4b8c00fa96b3f1
Andi Sidwell <andi@takkaria.org>
2016-02-08 19:16:07 +0000

    Add failing event adding test

--- test/integration/event-add.js
+++ test/integration/event-add.js
@@ -99,8 +99,9 @@ describe('Given I visit /event/add', function() {
 			browser.wait(done);
 		});
 
-		it('should return to the home page', function() {
+		it('should return to the home page, noting success', function() {
 			browser.assert.url('/');
+			browser.assert.text('alert', /event submitted/i)
 		});
 
 		it('should produce an event with a slug that includes its ID', function(done) {
