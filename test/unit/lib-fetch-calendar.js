57576617bf4928bd3b632ac6129b7a2474f5bac0
Andi Sidwell <andi@takkaria.org>
2016-09-05 01:08:36 +0100

    (new-fetch) Actually make an HTTP request

--- test/unit/lib-fetch-calendar.js
+++ test/unit/lib-fetch-calendar.js
@@ -3,8 +3,24 @@
 const expect = require('chai').expect;
 const fetchCalendar = require('../../lib/fetch-calendar');
 
+const nock = require('nock');
+nock.disableNetConnect();
+
+const CALENDAR_URL = 'http://example.net/ical';
+
 describe('fetch-calendar', function() {
 	it('must call the callback', function(done) {
 		fetchCalendar(null, done);
+	})
+
+	it('must make an HTTP request', function(done) {
+		let serverRequest = nock('http://example.net')
+			.get('/ical')
+			.replyWithFile(200, __dirname + '/calendar-data/cal001.ics');
+
+		fetchCalendar(CALENDAR_URL, function() {
+			expect(serverRequest.isDone()).to.equal(true);
+			done();
+		})
 	})
 })
