5fcf6cd47b7af4edfa7a057040033b4ec68fc2d9
Andi Sidwell <andi@takkaria.org>
2016-09-06 19:04:04 +0100

    Start adding basic HTTP support
    Include reporting of 301 redirects and some basic cache data

--- test/unit/lib-fetch-calendar.js
+++ test/unit/lib-fetch-calendar.js
@@ -11,10 +11,12 @@ nock.disableNetConnect();
 const CALENDAR_URL = 'http://example.net/ical';
 
 // Utility functions
-function fakeServer(filename) {
+function fakeServer(filename, headers) {
 	return nock('http://example.net')
 			.get('/ical')
-			.replyWithFile(200, __dirname + '/calendar-data/' + filename);
+			.replyWithFile(200,
+					__dirname + '/calendar-data/' + filename,
+					headers);
 }
 
 const models = require('../../models');
@@ -54,6 +56,55 @@ describe('fetch-calendar', function() {
 				expect(events.length).to.equal(1);
 				expect(allValidKeys(events[0])).to.equal(true);
 				done();
+			})
+		})
+
+		describe('and the server replies with caching headers', function() {
+			it('(served with etag) should return caching data', function(done) {
+				let serverRequest = fakeServer('cal001.ics', {
+					'ETag': '"TESTING"',
+				});
+
+				fetchCalendar(CALENDAR_URL, function(err, events, cacheData) {
+					expect(serverRequest.isDone()).to.equal(true);
+					expect(cacheData).to.deep.equal({
+						'etag': '"TESTING"',
+					});
+
+					done();
+				})
+			})
+
+			it('(served with last-modified) should return caching data', function(done) {
+				let serverRequest = fakeServer('cal001.ics', {
+					'Last-Modified': 'Sun, 04 Sep 2016 22:49:51 GMT'
+				});
+
+				fetchCalendar(CALENDAR_URL, function(err, events, cacheData) {
+					expect(serverRequest.isDone()).to.equal(true);
+					expect(cacheData).to.deep.equal({
+						'last-modified': 'Sun, 04 Sep 2016 22:49:51 GMT'
+					});
+
+					done();
+				})
+			})
+		})
+
+		describe('and the server replies with a redirect', function() {
+			it('it should return the new URL', function(done) {
+				let serverRequestRedirect = nock('http://example.net/')
+						.get('/redirect')
+						.reply(301, '', { Location: '/ical' });
+				let serverRequestReal = fakeServer('cal001.ics');
+
+				fetchCalendar('http://example.net/redirect', function(err, events, cacheData, newUrl) {
+					expect(serverRequestRedirect.isDone()).to.equal(true);
+					expect(serverRequestReal.isDone()).to.equal(true);
+					expect(newUrl).to.equal(CALENDAR_URL);
+
+					done();
+				})
 			})
 		})
 	})
